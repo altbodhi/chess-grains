91ce207857929e16b102cd9e15a4a507c0066b44
altbodhi <blevaka@gmail.com>
2021-06-09 14:57:36 +0700

    change comment for constant

--- chess-grains.lisp
+++ chess-grains.lisp
@@ -1,8 +1,8 @@
 ;;;; на первое поле шахматной доски кладем 1 зерно, на следующее в 2-а раза больше и т.д.
 (in-package :chess-grains)
 
-;;;; масса одного зерна пшеницы в кг
-(defconstant mass-bread-grain 0.000035)
+(defconstant mass-bread-grain 0.000035 
+    "масса одного зерна пшеницы в кг")
 (defun get-mass-grains ()
     (defun count-grains (i a r)
       (if (> i 64) r
